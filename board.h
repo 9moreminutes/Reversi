9745d8000db010ae890b95330c804ee16c7fc0e7
ktuazon <ktuazon@Katrinas-MacBook-Pro.local>
2013-10-12 15:14:37 -0500

    game mechanics

---
+++ board.h
@@ -0,0 +1,61 @@
+#ifndef _board_h
+#define _board_h
+
+#include <vector>
+#include <stack>
+#include <iostream>
+
+#include "Space.h"
+
+using namespace std;
+
+class GameEngine{
+private:
+    vector <vector <Space> > board;
+    
+    stack <Space> moves;
+    
+public:
+    //constructor, initializes board
+    GameEngine(){
+        for(int i = 1; i <= 8; ++i){
+            vector<Space> columns;
+            board.push_back(columns);
+            for(int j = 1; j <= 8; ++j){
+                if((i == 4 && j == 5) || (i == 5 && j ==4)){
+                    Space s = Space(WHITE, i, j);
+                    board[i-1].push_back(s);
+                }else if(((i == 4 && j == 4) || (i == 5 && j == 5))){
+                    Space s = Space(BLACK, i, j);
+                    board[i-1].push_back(s);
+                }else {
+                    Space s = Space(EMPTY, i, j);
+                    board[i-1].push_back(s);
+                }
+            }
+        }
+    }
+    
+    //push and pop functions to modify stack
+    Space popMove(){
+        Space s = moves.top();
+        return s;
+        moves.pop();
+    }
+    void pushMove(Space s){
+        moves.push(s);
+    }
+    
+    
+    bool checkIfValid(Space s, Tile player);
+    void move(Space s, Tile Player);
+    void undo(); // pops move from stack and undoes
+    vector<Space> showPossibleMoves(Tile player); //if empty, no moves
+    int tallyPoints();
+    void resetGame();
+    bool checkDirection(int row, int column, int xdirec, int ydirec, Tile enemycolor);
+    void drawBoard();
+    void setSpace(Tile t, int row, int column);
+};
+
+#endif
