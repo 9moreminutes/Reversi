3f701f27416343e17c62ce549d18d82b40a4d6c3
Matthew Horton <mtt.horton@gmail.com>
2013-10-16 16:19:54 -0500

    Create example

---
+++ example.cpp
@@ -0,0 +1,19 @@
+//EASY depth 1
+//MEDIUM depth 3
+//HARD depth 5
+final int values[8][8] = {}
+
+Space chooseMove(GameEngine game, Tile player, int depth)
+{
+	
+	if depth < 1
+		return
+
+		vector<Space> moves = game.getPossibleMoves(player);
+		if isTurn(player)
+			//do best move
+		else
+			//doworst move
+
+
+}
